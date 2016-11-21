8dfbccd5035c9ae7e7a5e552ba7e9ad60e0d3d0e
Ikechukwu Francis Uwazurike <uwazurike@gmail.com>
2016-11-21 10:43:50 -0800

    Initial Commit

---
+++ weatherApiApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using weatherApiApp.Models;
+
+namespace weatherApiApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("weather2");
+        }
+
+        public JsonResult GetWeather()
+        {
+            WeatherClass myWeather = new WeatherClass();
+            return Json(myWeather.GetCurrentWeather(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ weatherApiApp/Models/WeatherClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace weatherApiApp.Models
+{
+    public class WeatherClass
+    {
+        public object GetCurrentWeather()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Detroit&APPID=f98f62d8d7b41b43e42d474c68c6bc8d&units=imperial";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<object>(content);
+
+            return jsonContent;
+        }
+    }
+}
